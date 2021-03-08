ee1e16d8b549f4d986134348d231924d6df7f1ab
Max <maxim.macari.dam@gmail.com>
2021-03-08 15:30:29 +0100

    Initial Commit

---
+++ InterestCalculator/InterestCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestCalculatorApp.swift
+//  InterestCalculator
+//
+//  Created by Maxim Macari on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct InterestCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
